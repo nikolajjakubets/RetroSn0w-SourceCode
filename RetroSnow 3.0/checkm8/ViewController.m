d120fc06fcccb5cff31d006026ab719d4dcc7c6d
80036nd <40246027+80036nd@users.noreply.github.com>
2019-11-23 10:25:50 -0500

    Add files via upload

---
+++ RetroSnow 3.0/checkm8/ViewController.m
@@ -0,0 +1,44 @@
+#import "ViewController.h"
+#import <QuartzCore/QuartzCore.h>
+
+
+@implementation ViewController
+- (IBAction)update:(id)sender {
+    system("rm -rf ~/Documents/RetroSn0w");
+    system("rm -rf ~/Documents/IPWNDFU ~/Documents/checkm8gui/irecovery/");
+    system("git clone https://github.com/Nick80036nd/Pluvia  ~/Documents/RetroSn0w/");
+    system("git clone https://github.com/Nick80036nd/ipwndfu ~/Documents/IPWNDFU/");
+}
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.]
+    
+    [[_makesn0w cell] setBackgroundColor:[NSColor systemBlueColor]];
+    [[_updatebutton cell] setBackgroundColor:[NSColor systemBlueColor]];
+    [[_recovery cell] setBackgroundColor:[NSColor systemBlueColor]];
+    [[_pwnxout cell] setBackgroundColor:[NSColor systemBlueColor]];
+    [[_autobootout cell] setBackgroundColor:[NSColor systemBlueColor]];
+
+
+}
+
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+- (IBAction)Sn0w:(id)sender {
+    system("hmod +x ~/Documents/checkm8gui/irecovery/irecovery; cd ~/Documents/checkm8gui/ipwndfu/; ~/Documents/checkm8gui/ipwndfu/ipwndfu -p");
+    NSAlert *alert = [NSAlert alertWithMessageText:@"Maybe Done? (this is beta software)" defaultButton:@"Ok" alternateButton:nil otherButton:nil informativeTextWithFormat:@"I hope it worked!"];
+    [alert runModal];
+}
+- (IBAction)pwnx:(id)sender {
+    system("cd ~/Documents/checkm8gui/ipwndfu/; ~/Documents/checkm8gui/ipwndfu/ipwndfu -x");
+    NSAlert *alert = [NSAlert alertWithMessageText:@"Maybe Done? (this is beta software)" defaultButton:@"Ok" alternateButton:nil otherButton:nil informativeTextWithFormat:@"I hope it worked!"];
+    [alert runModal];
+}
+
+
+@end
