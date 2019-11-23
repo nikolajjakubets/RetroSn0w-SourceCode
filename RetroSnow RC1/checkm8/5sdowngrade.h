f245187b51873dbc3fd80137130884f6f3845271
80036nd <40246027+80036nd@users.noreply.github.com>
2019-11-23 10:33:02 -0500

    Add files via upload

---
+++ RetroSnow RC1/checkm8/5sdowngrade.h
@@ -0,0 +1,17 @@
+//
+//  5sdowngrade.h
+//  checkm8
+//
+//  Created by Nick Drotleff on 10/13/19.
+//  Copyright © 2019 80036nd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface _sdowngrade : NSViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RetroSnow RC1/checkm8/iPhone4Restore.h
@@ -0,0 +1,17 @@
+//
+//  iPhone4Restore.h
+//  checkm8
+//
+//  Created by Nick Drotleff on 10/1/19.
+//  Copyright © 2019 80036nd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface iPhone4Restore : NSViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
